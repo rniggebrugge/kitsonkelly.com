11e110b69b8d2c6ded646f4423c728c3e075dba5
Kitson Kelly <kitson.kelly@asseverate.co.uk>
2012-05-10 19:32:33 -0700

    fixed typo

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var http = require('http');
 
-var server = http.createSever(function(req, res){
+var server = http.createServer(function(req, res){
 	res.writeHead(200, { "Content-Type": "text/plain" });
 	res.writeEnd("Hello world\n");
 });
