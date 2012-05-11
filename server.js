4fbae4708b94c79a6ed7b786bccf4809f00e34a5
Kitson Kelly <kitson.kelly@asseverate.co.uk>
2012-05-11 18:45:30 +0100

    updating app

--- server.js
+++ server.js
@@ -1,8 +1,31 @@
-var http = require('http');
+/*!
+ * kitsonkelly.com
+ * Server Module
+ */
 
-var server = http.createServer(function(req, res){
-	res.writeHead(200, { "Content-Type": "text/plain" });
-	res.end("Hello world\n");
+var util = require('util'),
+    express = require('express');
+
+var app = express.createServer(),
+	appPort = process.env.PORT || 8001;
+
+app.configure(function(){
+	app.set('view options', { layout: false });
+	app.use(express.cookieParser());
+	app.use(express.session({ secret: 'yHCoyEPZ9WsNDORGb9SDDMNn0OOMcCgQiW5q8VFhDHJiztvvVVCPkZQWUAXl' }));
+	app.use(app.router);
+	app.use('/lib/dojo', express.static('../dojo'));
+	app.use('/lib/dijit', express.static('../dijit'));
+	app.use('/lib/dojox', express.static('../dojox'));
+	app.use('/lib', express.static('./lib'));
+	app.use('/css', express.static('./css'));
+	app.use('/images', express.static('./images'));
 });
 
-server.listen(process.env.PORT || 8001);
+app.get('/', function(request, response, next){
+	response.render('404.jade', { status: 404, res: request.url });
+})
+
+app.listen(appPort);
+
+util.puts('HTTP server started on port ' + appPort);
